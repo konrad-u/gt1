504d053d04103cd5f2f083572115b9bfb6370a75
Konrad <konradaukens@gmail.com>
2022-04-30 21:50:35 +0200

    handed in assignment without comments
    needs fixing of steering behavior and need to start on new assignment

--- src/s0572411/W1.java
+++ src/s0572411/W1.java
@@ -2,6 +2,7 @@ package s0572411;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.awt.geom.Path2D;
 
 import lenz.htw.ai4g.ai.AI;
 import lenz.htw.ai4g.ai.DivingAction;
@@ -10,6 +11,16 @@ import lenz.htw.ai4g.ai.PlayerAction;
 
 public class W1 extends AI {
 
+	Point[] pearlPoints = info.getScene().getPearl();
+	Point[] visitedPearls = new Point[pearlPoints.length];
+	Path2D[] obstacles = info.getScene().getObstacles();
+	float maxVel = info.getMaxVelocity();
+	Point playerPos;
+	int pearlCount;
+	int obsDistance = 5;
+	int changeDivision = 10;
+	
+
 	public W1(Info info) {
 		super(info);
 		enlistForTournament(572411);
@@ -17,7 +28,7 @@ public class W1 extends AI {
 
 	@Override
 	public String getName() {
-		return "AI Taucher";
+		return "Tomitaro Fujii";
 		
 	}
 
@@ -33,32 +44,86 @@ public class W1 extends AI {
 
 	@Override
 	public PlayerAction update() {
+		playerPos = new Point((int)info.getX(), (int)info.getY());
+		checkIfPlayerReachedPearl(); 
 		//return new DivingAction(1.0f,3.14f);
-		return persueClosestPearl();
+		return pursueClosestPearl();
 	}
 	
-	public DivingAction persueClosestPearl() {
-		Point[] pearlPoints = info.getScene().getPearl();
-		Point playerPos = new Point((int)info.getX(), (int)info.getY());
+	//own methods 
+	
+	public DivingAction pursueClosestPearl() {
 		
-		Point closestPearl = closestPoint(pearlPoints, playerPos);
+		Point closestPearl = getClosestPearl(pearlPoints, playerPos);
 		
-		Point directionPoint = fromStartToGoal(playerPos, closestPearl);
+		Point directionPoint = pointFromStartToGoal(playerPos, closestPearl);
 		
-		float directionToPearl = (float)Math.atan2(directionPoint.y,directionPoint.x);
+		float[] seekNormPoints = normalizePointToFloatArray(directionPoint);
 		
-		//insert 2 methods; one for calculating flee Diving action, 
-		//one for balancing this against the seek diving action
+		float directionToPearl = -(float)Math.atan2(seekNormPoints[1],seekNormPoints[0]);
 		
-		return new DivingAction(1.0f, -directionToPearl);
+		DivingAction pearlPursuit = new DivingAction(maxVel, directionToPearl);
+		
+		Point obstacleAvoidance = getFleeFromObstacle();
+		
+		if(obstacleAvoidance != null) {
+			
+			int newX = directionPoint.x + obstacleAvoidance.x;
+			int newY = directionPoint.y + obstacleAvoidance.y;
+		
+			Point compromisePoint = new Point(newX, newY);
+		
+			float[] compNormPoints = normalizePointToFloatArray(compromisePoint);
+		
+			float compDirToPearl = -(float)Math.atan2(compNormPoints[0],compNormPoints[1]);
+		
+			DivingAction compPursuit = new DivingAction(maxVel, compDirToPearl);
+			
+			return compPursuit;
+			
+		}
+		
+		return pearlPursuit;
 	}
 	
-	public Point addPoints(Point point1, Point point2) {
-		Point summedPoint = new Point ((point1.x + point2.x), (point1.y + point2.y));
-		return summedPoint;
+	public float adjustAngle(float desiredAngle, float degreeOfAdjustment) {
+		
+		float adjustedAngle;
+		
+		float desiredAngleDegrees = (float) (desiredAngle*(180/Math.PI));
+		if(desiredAngleDegrees < 0) {
+			desiredAngleDegrees = desiredAngleDegrees + 360;
+		}
+		
+		float currentAngle = info.getOrientation();
+		
+		float currentAngleDegrees = (float) (currentAngle*(180/Math.PI));
+		if(currentAngleDegrees < 0) {
+			currentAngleDegrees = currentAngleDegrees + 360;
+		}
+		
+		if(currentAngleDegrees < desiredAngleDegrees) {
+			adjustedAngle = (float) (currentAngleDegrees - (desiredAngleDegrees/degreeOfAdjustment*Math.PI/180));
+			return adjustedAngle;
+		}
+		if(currentAngleDegrees > desiredAngleDegrees) {
+			adjustedAngle = (float) (currentAngle + (desiredAngle/degreeOfAdjustment*Math.PI/180));
+			return adjustedAngle;
+		}
+		return (Float) null;
 	}
 	
-	public Point fromStartToGoal(Point start, Point goal) {
+	public DivingAction averageTwoDAs(DivingAction d1, DivingAction d2) {
+		
+		float avgVel = ((d1.getPower()) + d2.getPower())/2;
+		float avgDir = ((d1.getDirection()) + (d2.getDirection()));
+		
+		DivingAction averagedAction = new DivingAction(avgVel, avgDir);
+		
+		return averagedAction;
+	}
+	
+	public Point pointFromStartToGoal(Point start, Point goal) {
 		Point directionPoint = new Point ((goal.x - start.x),(goal.y - start.y));
 		return directionPoint;
 	}
@@ -66,14 +131,15 @@ public class W1 extends AI {
 	public float calculateDistance(Point start, Point goal) {
 		float distance = 0;
 		
-		Point distancePoint = fromStartToGoal(start, goal);
+		Point distancePoint = pointFromStartToGoal(start, goal);
 		
 		distance = (float)Math.sqrt(distancePoint.x*distancePoint.x + distancePoint.y*distancePoint.y);
 		
 		return distance;
 	}
+	
+	public Point getClosestPearl (Point[] pearls, Point player) {
 
-	public Point closestPoint (Point[] pearls, Point player) {
 		Point closestPearl = new Point();
 		float closestPearlDistance = 100000;
 		for(int i = 0; i < pearls.length; i++) {
@@ -86,8 +152,48 @@ public class W1 extends AI {
 		return closestPearl;
 	}
 	
-	public void printSceneInfo() {
-		System.out.print("thing");
-		System.out.print(info);
+	public Point getFleeFromObstacle() {
+		Point[] playerSurr = getPointSurrounding(obsDistance, playerPos);
+		for(int i = 0; i < obstacles.length; i++) {
+			for(int j = 0; j < playerSurr.length; j++) {
+				if(obstacles[i].contains(playerSurr[j])) {
+					Point directionPointFromObstacle = pointFromStartToGoal(playerPos, playerSurr[j]);
+					return directionPointFromObstacle;
+				}
+			}
+		}
+		return null;
 	}
+	
+	public Point[] getPointSurrounding(int distanceFromPlayer, Point pointOfSurrounding) {
+		int i = distanceFromPlayer;
+		Point[] playerSurrounding = new Point[8];
+		playerSurrounding[0] = new Point (pointOfSurrounding.x+i, pointOfSurrounding.y);
+		playerSurrounding[1] = new Point (pointOfSurrounding.x-i, pointOfSurrounding.y);
+		playerSurrounding[2] = new Point (pointOfSurrounding.x+i, pointOfSurrounding.y+i);
+		playerSurrounding[3] = new Point (pointOfSurrounding.x+i, pointOfSurrounding.y-i);
+		playerSurrounding[4] = new Point (pointOfSurrounding.x-i, pointOfSurrounding.y+i);
+		playerSurrounding[5] = new Point (pointOfSurrounding.x-i, pointOfSurrounding.y-i);
+		playerSurrounding[6] = new Point (pointOfSurrounding.x, pointOfSurrounding.y+i);
+		playerSurrounding[7] = new Point (pointOfSurrounding.x, pointOfSurrounding.y-i);
+		return playerSurrounding;
+	}
+
+	public void checkIfPlayerReachedPearl() {
+		for(int i = 0; i < pearlPoints.length; i++) {
+			if(calculateDistance(pearlPoints[i], playerPos) < 5) {
+				pearlPoints[i] = new Point(99999999, 999999999);
+				return;
+			}
+		}
+	}
+
+	public float[] normalizePointToFloatArray(Point point) {
+		float normPoint[] = new float[2];
+		float pointVectorLength = calculateDistance(playerPos, point);
+		normPoint[0] = (point.x/pointVectorLength);
+		normPoint[1] = (point.y/pointVectorLength);
+		return normPoint;
+	}
+
 }
